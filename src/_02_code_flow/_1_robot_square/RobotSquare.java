9293b4b5fecffa3dd6df6c1bb3b8399edcfb90be
root <root@74f0f564ed70>
2020-06-11 23:56:12 +0000

    Finish robot octagon

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -17,22 +17,23 @@ public class RobotSquare {
 
         // Put the robot's pen down so it can draw
 		//        Use the     rob.penDown();   command.
-
+        rob.penDown();
         // SPEED. Make the robot move quickly.
+    	
 		//        Use the     rob.setSpeed();   command.
-
-
-        // DRAW.  Draw a line
+        rob.setSpeed(10);
+int sides=1;
+        // ;;;;.  Draw a line
     	//		Use the     rob.move(); command
-
-
+     while (sides<9)  {       rob.move(100);    
+sides = sides+1;
 		// TURN.  Turn 90 degrees
     	//      Use the     rob.turn();  command
-
+        rob.turn(45); }
 
         // LOOP.  Now put the DRAW and TURN code into a for loop to repeat 4 times.
 		//        Did your Robot draw a square?
-
+        
 
     }
 }
