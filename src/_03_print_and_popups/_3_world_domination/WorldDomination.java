f7aff59536fda756c890e1beac8df3c68fa94682
root <root@6e80a17dea1c>
2020-07-02 23:51:33 +0000

    world domination

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -6,6 +6,8 @@ import javax.swing.JOptionPane;
 public class WorldDomination {
 	
 	public static void main(String[] args) {
+		String QandA= JOptionPane.showInputDialog("do you know how to write code");
+		JOptionPane.showMessageDialog(null, "you will rule the world");
 		
 		// 1. Ask the user if they know how to write code.
 
