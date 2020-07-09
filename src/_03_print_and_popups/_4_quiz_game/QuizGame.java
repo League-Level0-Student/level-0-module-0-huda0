a35ff8c040061fcc19882ed494957b849f7a4b6e
root <root@d25aa8dbbd92>
2020-07-09 23:59:32 +0000

    quiz game

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,50 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 100;
 		// ASK A QUESTION AND CHECK THE ANSWER
+		String recorder = JOptionPane.showInputDialog("What is 1+1?");
 		
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
-				
+			
+		       if (recorder.equals("2")) {  
+		    	   System.out.println("print");
+		    	   score++;
+		    	   
+		    	   } 
+		       recorder = JOptionPane.showInputDialog("What sound does a cow make?");
+		       
+		       if (recorder.equals("moo")) {
+		    	   
+		       }
+		       
+		       recorder = JOptionPane.showInputDialog("How many colors are in the rainbow?");
+		    		   
+		    	if (recorder.equals("7"))	{
+		    		
+		    	}
+		       
+		       recorder = JOptionPane.showInputDialog("What is the name of Harry Potter's Owl?");
+		    		   
 				// 4.  if the user's answer was correct, add one to their score 
 		
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
-		
+		/*
+		 * What is 1+1?
+		 * What sound does a cow make?
+		 * How many colors are in the rainbow?
+		 * What is the name of Harry Potter's owl?
+		 */
 		
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "your final score is " + score);
 	}
 }
